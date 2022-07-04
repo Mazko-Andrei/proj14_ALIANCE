a6ac7438b8eb196ac3eb3736b1377a178b5929e0
Mazko-Andrei <103064672+Mazko-Andrei@users.noreply.github.com>
2022-07-04 09:43:56 +0300

    changed the file contact.menu.js

--- js/contract-menu.js
+++ js/contract-menu.js
@@ -3,9 +3,13 @@ const linkMenu = document.querySelector("#contract-menu");
 const menu = document.querySelector(".contract-menu");
 const body = document.body;
 
-linkMenu.addEventListener("click", function(e){
+linkMenu.addEventListener("mousemove", function(e){
     e.stopPropagation();
-    menu.classList.toggle("active");
+    menu.classList.add("active");
+})
+linkMenu.addEventListener("mouseout", function(e){
+    e.stopPropagation();
+    menu.classList.remove("active");
 })
 window.addEventListener("click", function(){
     if (menu.classList.contains('active')){
@@ -19,17 +23,3 @@ menu.addEventListener('click', function(event){
 
 
 
-const burgerKing = document.querySelectorAll(".burger_king");
-
-burgerKing.forEach(function(item){
-    item.addEventListener("mouseover", function () {
-        this.style = `
-        background-image: url(images/aerosolproducts/Burger_King_logo_hover.png)
-        `;
-    });
-    item.addEventListener("mouseout", function () {
-        this.style = `
-        background-image: url(images/aerosolproducts/Burger_King_logo.png)
-        `;
-    });
-});
